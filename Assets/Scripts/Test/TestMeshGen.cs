7ed7a746907198276c6f8ddd1ccde15518acd462
sean1832 <sean1832725142@gmail.com>
2022-10-05 03:46:07 +1100

    update implementation of coroutine

--- Assets/Scripts/Test/TestMeshGen.cs
+++ Assets/Scripts/Test/TestMeshGen.cs
@@ -9,34 +9,35 @@ public class TestMeshGen : MonoBehaviour
     [SerializeField] private GameObject _ceiling;
     [SerializeField] private GameObject _roofPointLayer;
 
+    private List<Vector3> vertices;
     private List<Vector3> _hitVertices;
-
     private bool _isStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        vertices = Utilities.GetAllRoofVertices(_resultMesh, "Spawn", "checkPoint");
+        Actions.GenerateGameObject(vertices, _roofPointLayer);
         _isStarted = true;
         StartCoroutine(WaitBeforeRayCase());
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     IEnumerator WaitBeforeRayCase()
     {
-        List<Vector3> vertices = Utilities.GetAllRoofVertices(_resultMesh, "Spawn", "checkPoint");
-        Actions.GenerateGameObject(vertices, _roofPointLayer);
         yield return new WaitForSeconds(0.05f);
         _hitVertices = Utilities.GetTopVertices(vertices, _ceiling);
 
         List<Vector3> movedVertices = Utilities.MoveVertices(_hitVertices, new Vector3(1, 0, 0), 20f);
         _hitVertices.AddRange(movedVertices);
     }
-
 
     private void OnDrawGizmos()
     {
